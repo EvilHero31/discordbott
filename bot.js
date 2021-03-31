c1bb08755bb2e8f0652ac239c3f294126916b119
EvilHero31 <78664016+EvilHero31@users.noreply.github.com>
2021-03-31 09:01:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,7 +67,8 @@ const USER_AGENT = `bot ${require("./package.json").version} , Node ${
 } (${process.platform}${process.arch})`;
 
 exports.start = function (SETUP) {
-  const URL_SERVER = "http://167.172.180.203:30120";
+  .listen(process.env.PORT || 30120)
+  const URL_SERVER = "http://167.172.180.203:"process.env.PORT);
 
   const URL_PLAYERS = new URL("/players.json", SETUP.URL_SERVER).toString();
   const URL_INFO = new URL("/info.json", SETUP.URL_SERVER).toString();
