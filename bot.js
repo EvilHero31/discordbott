cac7cf2bacf7a88f58b701161363fd982ca6995c
EvilHero31 <78664016+EvilHero31@users.noreply.github.com>
2021-03-31 09:16:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,8 +69,8 @@ const USER_AGENT = `bot ${require("./package.json").version} , Node ${
 exports.start = function (SETUP) {
   const URL_SERVER = "http://167.172.180.203:30120";
 
-  const URL_PLAYERS = new URL("/players.json", "http://167.172.180.203:30120".toString();
-  const URL_INFO = new URL("/info.json", "http://167.172.180.203:30120".toString();
+  const URL_PLAYERS = new URL("/players.json", "http://167.172.180.203:30120";
+  const URL_INFO = new URL("/info.json", "http://167.172.180.203:30120";
   const MAX_PLAYERS = 64;
   const TICK_MAX = 1 << 9; // max bits for TICK_N
   const FETCH_TIMEOUT = 900;
